cbbd4b6397c77af2e2c18de38b81b36e74e43cb5
nkl_alexis <nklbigone@gmail.com>
2020-11-02 18:06:52 +0200

    room booking

---
+++ src/database/migrations/20201102123337-create-booking.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      check_in_date: {
+        type: Sequelize.DATE
+      },
+      check_out_date: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Bookings');
+  }
+};
