3d2da4458940179d9befe641e7598510a00cc773
Alain MUCYO <alainmucyo3@gmail.com>
2020-11-08 15:23:31 +0200

    Remove some comments

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -9,16 +9,16 @@ import roomTest from './room/room.test';
 import userRoles from './roles.test';
 
 describe("Barefoot Nomad: ", () => {
-/*  describe("Trip CRUD: ", tripTests);
-  describe("Email login: ", loginTests); */
+  describe("Trip CRUD: ", tripTests);
+  describe("Email login: ", loginTests);
   describe("User roles: ", userRoles);
-/*  describe("Social login: ", oauthTest);
+  describe("Social login: ", oauthTest);
   describe("Accommodation: ", accommodationTest);
-  describe("Room: ", roomTest); */
+  describe("Room: ", roomTest);
 });
 
 // USER LOGOUT TEST SUIT
 
-/*describe("Logout test suit", () => {
+describe("Logout test suit", () => {
   describe("POST api/user/logout logging out user", logout);
-});*/
+});
