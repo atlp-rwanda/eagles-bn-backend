2d7730e0425a39a209b3fd8152c6b8bfa377c801
RedJanvier <janvierntwali@gmail.com>
2020-11-13 07:46:48 +0200

    fix(redis): configuration data

--- src/config/redis_config.js
+++ src/config/redis_config.js
@@ -1,7 +1,11 @@
+/* eslint-disable import/no-mutable-exports */
 import redis from 'redis';
 
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PORT = process.env.REDIS_URL || 6379;
+let client = redis.createClient(REDIS_PORT);
 
-const client = redis.createClient(REDIS_PORT);
+if (process.env.REDIS_URL) {
+  client = redis.createClient(process.env.REDIS_URL);
+}
 
 export default client;
