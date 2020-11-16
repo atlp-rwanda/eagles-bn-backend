786f99d1d9a0f087a196bdb79ac29ab6eb5b5841
siyubu <solangeiyubu@gmail.com>
2020-11-16 12:02:51 +0200

    accommodation like/unlike and feedback documentation fix

--- src/routes/accomodation.js
+++ src/routes/accomodation.js
@@ -1,6 +1,7 @@
 import Router from 'express';
 import AccomodationController from '../controllers/accomodation';
 import accommodationValidator from '../middlewares/accommodation';
+import feedbackValidation from "../validators/feedback"
 import verifyAccessToken from '../middlewares/verifyToken';
 import user from '../validators/user';
 import { roles } from '../helpers/roles';
@@ -31,6 +32,6 @@ router.delete(
   catcher(AccomodationController.destroy)
 );
 router.post("/:id/like", verifyAccessToken, AccomodationController.like);
-router.post("/:id/feedback", verifyAccessToken, AccomodationController.feedback);
+router.post("/:id/feedback", verifyAccessToken,feedbackValidation,AccomodationController.feedback);
 
 export default router;
